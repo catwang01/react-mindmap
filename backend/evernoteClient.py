349ee904c6db32130ebf1673428fd1745ce2d20e
catwang01 <edwardelricwzx@gmail.com>
2022-10-15 13:19:30 +0100

    1. better logging 2. use notestore for each request

--- backend/evernoteClient.py
+++ backend/evernoteClient.py
@@ -8,7 +8,6 @@ evernote_token = os.environ.get('EVERNOTE_TOKEN', None)
 assert  evernote_token is not None, 'Please set token with "export EVERNOTE_TOKEN=<your_token>"'
 # Set up the NoteStore client
 client = EvernoteClient(token=evernote_token, china=True ,sandbox=False)
-notestore = client.get_note_store()
 app = Flask(__name__)
 
 def extractNote(note):
@@ -27,8 +26,19 @@ def extractNotebook(notebook):
         'guid': notebook.guid,
     }
 
+def helper(func):
+    def wrapper(*args, **kwargs):
+        app.logger.debug("request.form: %s", request.form)
+        ret = func(*args, **kwargs)
+        response, status_code = ret
+        app.logger.debug("response.get_data(): %s", response.get_data())
+        return ret
+    return wrapper
+
+@helper
 @app.route('/getNotebooks', methods=['GET', 'POST'])
 def getNotebooks():
+    notestore = client.get_note_store()
     status_code = 200
     try:
         notebookList = notestore.listNotebooks()
@@ -41,14 +51,17 @@ def getNotebooks():
             'notebooks': [extractNotebook(nb) for nb in notebookList]
         })
     response.headers.add('Access-Control-Allow-Origin', '*')
+    app.logger.debug("response.get_data(): %s", response.get_data())
     return response, status_code
 
 @app.route('/test')
 def test():
     return "hello! this is test"
 
+@helper
 @app.route('/getNote', methods=['GET', 'POST'])
 def getNote():
+    notestore = client.get_note_store()
     guid = request.form.get('guid')
     status_code = 200
     if guid is None:
@@ -63,29 +76,28 @@ def getNote():
                 'error': str(e)
             })
         else:
-            print(note)
             response = flask.jsonify({
                 'note':  extractNote(note)   
             })
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response, status_code
 
+@helper
 @app.route('/findNotes', methods=['GET', 'POST'])
 def findNotes():
-    print(request.form)
+    notestore = client.get_note_store()
     status_code = 200
     note_filter = NoteStore.NoteFilter()
     if request.form.get('filter_order', None):
         note_filter.order = int(request.form.get('filter_order'))
         note_filter.ascending = 0
+    start = int(request.form.get('start', '0'))
+    end =  int(request.form.get('end', '10'))
     try:
-        notes = notestore.findNotes(note_filter, 
-                                    int(request.form.get('start', '0')), 
-                                    int(request.form.get('offset', '10'))).notes
+        notes = notestore.findNotes(note_filter, start, end).notes
     except Exception as e:
         response, status_code = flask.jsonify({ 'error': str(e) }), 400
     else:
-        print(notes)
         res = []
         for note in notes:
             res.append(extractNote(note))
