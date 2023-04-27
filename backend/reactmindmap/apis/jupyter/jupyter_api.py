c774c7e4c23794f02554efab8d9e0670a2bd0107
catwang01 <edwardelricwzx@gmail.com>
2023-04-27 08:32:46 +0000

    add parents parameter for create_notebook

--- backend/reactmindmap/apis/jupyter/jupyter_api.py
+++ backend/reactmindmap/apis/jupyter/jupyter_api.py
@@ -1,4 +1,5 @@
 from jupyter.jupyter_client import JupyterClient
+from retrying import retry
 from flask import request, jsonify
 from reactmindmap.apis.jupyter.template import get_template
 from reactmindmap.app import app
@@ -21,14 +22,25 @@ def create_directory():
     else:
         return jsonify({'message': 'Create succeeded!'}), 200
 
+def get_parent_path(path) -> str:
+    splitted = path.strip().split('/')[:-1]
+    if len(splitted) == 0:
+        return "/"
+    else:
+        return "/".join(splitted)
+
 @app.route("/api/jupyter/create_notebook", methods=["POST"])
 def create_notebook():
     app.logger.debug(request.get_json())
     path = request.get_json()["path"]
     template_name = request.get_json().get("template_name", "basic")
     note_title = request.get_json().get("note_title", "Untitled")
+    parents = request.get_json().get("parents", False)
     interpolated = get_template({"note_title": note_title}, template_name)
     try:
+        if parents:
+            parent_path = get_parent_path(path)
+            retry(stop_max_attempt_number=3)(jupyter_client.create_directory_recursive)(parent_path)
         jupyter_client.create_notebook(path, interpolated)
     except Exception as e:
         return jsonify({'message': 'Create failed!'}), 400
