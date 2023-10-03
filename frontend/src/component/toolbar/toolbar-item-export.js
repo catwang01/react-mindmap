802ced898fa87e4e587c91bf7baa6b8aa87b3fa6
catwang01 <edwardelricwzx@gmail.com>
2023-10-03 17:18:12 +0800

    optimize toolbar-item-export.js

--- frontend/src/component/toolbar/toolbar-item-export.js
+++ frontend/src/component/toolbar/toolbar-item-export.js
@@ -1,33 +1,43 @@
 import cx from "classnames";
+import { memo } from "react";
 import { iconClassName } from "@blink-mind/renderer-react";
 import { Menu, MenuDivider, MenuItem, Popover } from "@blueprintjs/core";
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import { downloadFile } from "../../utils";
 
 export function ToolbarItemExport(props) {
-  const onClickExportJson = e => {
-    const { diagramProps } = props;
-    const { controller } = diagramProps;
-
-    const json = controller.run("serializeModel", { ...diagramProps, model: controller.currentModel }, );
+  console.log("ToolbarItemExportprops: ", { props })
+  const { diagramProps } = props;
+  const { controller } = diagramProps;
+  const onClickExportJson = useCallback(e => {
+    const json = controller.run("serializeModel",
+      {
+        ...diagramProps,
+        model: controller.currentModel
+      }
+    );
     const jsonStr = JSON.stringify(json);
+    const model = controller.currentModel;
     const url = `data:text/plain,${encodeURIComponent(jsonStr)}`;
     const title = controller.run("getTopicTitle", {
       ...diagramProps,
-      topicKey: controller.currentModel.rootTopicKey
+      model,
+      topicKey: model.rootTopicKey
     });
     downloadFile(url, `${title}.blinkmind`);
-  };
+  }, [controller]);
+  return <ToolbarItemExportPopOver onClick={onClickExportJson} />
+}
 
-  return (
+export const ToolbarItemExportPopOver = memo(
+  ({ onClick }) =>
     <div className={cx("bm-toolbar-item", iconClassName("export"))}>
       <Popover enforceFocus={false}>
         <div className="bm-toolbar-popover-target" />
         <Menu>
-          <MenuItem text="JSON(.json)" onClick={onClickExportJson} />
+          <MenuItem text="JSON(.json)" onClick={onClick} />
           <MenuDivider />
         </Menu>
       </Popover>
     </div>
-  );
-}
+);
