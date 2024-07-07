5df6bf47965c19c0256245a096037177d086d0be
catwang01 <edwardelricwzx@gmail.com>
2024-07-07 13:14:12 +0000

    update config

--- backend/reactmindmap/connections/config.py
+++ backend/reactmindmap/connections/config.py
@@ -1,10 +1,11 @@
 from reactmindmap.databases.mysql.connection import MysqlConnection
+import os
 
 CONFIG  = {
     "mysql": {
         "connectionClass": MysqlConnection,
         "connectionConfig": {
-            "host":"mysql",
+            "host": os.environ.get("DB_CONNECTION_NAME", "mysql"),
             "port": 3306,
             "user": "root",
             "password": "12345",
--- frontend/src/wdyr.js
+++ frontend/src/wdyr.js
@@ -1,7 +1,7 @@
 import React from 'react';
-if (process.env.NODE_ENV === 'development' && (process.env.REACT_APP_WDYR ?? false)) {
+if (process.env.NODE_ENV === 'development' && (process.env.REACT_APP_WDYR ?? true)) {
   const whyDidYouRender = require('@welldone-software/why-did-you-render');
   whyDidYouRender(React, {
-    trackAllPureComponents: true,
+    // trackAllPureComponents: true,
   });
 }
